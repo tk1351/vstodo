808bd1f5721d909bc1f1851290f9ff6ed71cf67c
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 08:34:30 +0900

    chore: typeormとpgの疎通設定

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    UsersModule,
+    TodosModule,
+  ],
 })
 export class AppModule {}
---
+++ api/src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const configService = new ConfigService();
+
+    return {
+      type: 'postgres',
+      host: configService.get('POSTGRES_HOST'),
+      port: 5432,
+      username: configService.get('POSTGRES_USER'),
+      password: configService.get('POSTGRES_PASSWORD'),
+      database: configService.get('POSTGRES_DB'),
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: configService.get('POSTGRES_SYNCHRONIZE'),
+    };
+  }
+}
