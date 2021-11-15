9c8d45a10c44f27cf064e434c5071ad3f20adf71
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 13:49:48 +0900

    fix: nestjsのcors設定

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: process.env.APP_URL });
   app.use(cookieParser());
   await app.listen(8080, '0.0.0.0');
 }
