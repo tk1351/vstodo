481c714d8f5b2c2ddac243ce8e8a3f8a99e8fc7a
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 11:10:26 +0900

    feat: todo作成機能追加

--- api/src/todos/todos.controller.ts
+++ api/src/todos/todos.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetCurrentUser } from '../auth/get-user.decorator';
+import { User } from '../users/models/user.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Post('/create')
+  @UseGuards(AuthGuard('jwt'))
+  create(
+    @Body() createTodoDto: CreateTodoDto,
+    @GetCurrentUser() user: User,
+  ): Promise<boolean> {
+    return this.todosService.createTodo(createTodoDto, user);
+  }
+}
--- api/src/todos/todos.module.ts
+++ api/src/todos/todos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { TodosRepository } from './todos.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TodosRepository])],
   providers: [TodosService],
-  controllers: [TodosController]
+  controllers: [TodosController],
 })
 export class TodosModule {}
