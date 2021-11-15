db7d65112e68745fc2db752872db90ee4d55c6c5
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 13:49:59 +0900

    feat: login機能作成

---
+++ app/styles/components/auth/login.styles.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react'
+
+export const container = css`
+  width: 60vw;
+  margin: 0 auto;
+`
---
+++ app/types/input.ts
@@ -0,0 +1,4 @@
+export type LoginInputsType = {
+  name: string
+  password: string
+}
