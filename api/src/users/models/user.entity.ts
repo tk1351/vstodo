653792b14348544457013cf72cc14e48acb6f038
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 09:24:45 +0900

    feat: ログイン機能追加

--- api/src/users/models/user.entity.ts
+++ api/src/users/models/user.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, OneToMany, Unique } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { DefaultEntity } from '../../entity';
 import { Todo } from '../../todos/models/todo.entity';
 
@@ -8,12 +9,17 @@ export class User extends DefaultEntity {
   @Column()
   name: string;
 
-  @Column()
+  @Column({ select: false })
   password: string;
 
-  @Column()
+  @Column({ select: false })
   salt: string;
 
   @OneToMany(() => Todo, (todos) => todos.user)
   todos: Todo[];
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './models/user.entity';
 import { UsersRepository } from './users.repository';
 
+export type JwtPayload = {
+  userId: number;
+};
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   async findUsers(): Promise<User[]> {
@@ -16,5 +22,17 @@ export class UsersService {
 
   async createUser(createUserDto: CreateUserDto): Promise<boolean> {
     return await this.usersRepository.createUser(createUserDto);
+  }
+
+  async login(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const userId = await this.usersRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+    if (!userId) throw new UnauthorizedException('認証情報が無効です');
+
+    const payload: JwtPayload = { userId };
+    const token = this.jwtService.sign(payload);
+
+    return token;
   }
 }
