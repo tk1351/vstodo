66ce34ae5c475ba76d5c7cfd8acf7d11397a8fe2
tk1351 <takakikitsui0111@gmail.com>
2021-11-15 13:09:36 +0900

    chore: Mui, emotion, recoil設定

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { RecoilRoot } from 'recoil'
+import createEmotionCache from '../src/createEmotionCache'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from '../src/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const MyApp = (props: MyAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <RecoilRoot>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
