c1b507f3109257b223d0177869db719851f72216
tk1351 <takakikitsui0111@gmail.com>
2021-11-16 09:19:17 +0900

    feat: Todo追加機能、追加後にTodosリフェッチ

--- app/src/api/todo.ts
+++ app/src/api/todo.ts
@@ -1,1 +1,9 @@
+import { CreateTodoDto } from '../../types/todo'
+import { api } from './api'
+
 export const FETCH_TODOS_API_URL = '/todos'
+
+export const addTodo = async (dto: CreateTodoDto) => {
+  const res = await api.post<boolean>('/todos/create', dto)
+  return res.data
+}
